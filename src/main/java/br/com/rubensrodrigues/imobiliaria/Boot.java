b96d09efad97ab43be7c35b9e20ec9090f60a5c9
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2019-02-01 01:30:44 -0200

    Teste de commit a partir do Eclipse

--- src/main/java/br/com/rubensrodrigues/imobiliaria/Boot.java
+++ src/main/java/br/com/rubensrodrigues/imobiliaria/Boot.java
@@ -10,6 +10,10 @@ public class Boot
    public static void main(String[] args)
    {
 	   System.out.println("teste");
+	   int a = 5;
+	   int b = 10;
+	   int c = a + b;
+	   System.out.println(c);
 	   SpringApplication.run(Boot.class, args);
    }
 }
